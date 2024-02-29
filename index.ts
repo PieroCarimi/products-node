67a24d6bb3cda51c70a2b62d1b11fdefb4f8df00
piero.carimi <pierocarimi@gmail.com>
2024-02-29 09:27:09 +0100

    feat(get): created endpoint to retrieve product data

---
+++ index.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = 3001;
+
+app.get('/api/products', async (req: Request, res: Response) => {
+    try {
+        const response = await fetch('https://mockend.up.railway.app/api/products');
+        const products = await response.json();
+        res.json(products);
+    }catch (error) {
+        res.status(500).json({ error: 'Internal Server Error '});
+    }
+});
+
+app.listen(port, () => {
+    console.log('Sever running at htpp://localhost:${port}');
+})
