ec20eaf89e4f1777a1d53ceb4e9782d14426a680
piero.carimi <pierocarimi@gmail.com>
2024-02-29 11:38:01 +0100

    feat(path): updated path

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fetch = require('node-fetch');
 const app = express();
 const port = 3001;
 
-app.get('/api/products', async (req, res) => {
+app.get('/', async (req, res) => {
   const response = await fetch('https://mockend.up.railway.app/api/products');
   const products = await response.json();
 
